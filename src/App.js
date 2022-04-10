e3b7aa78c5f7fd9fb4b46147ec97f38f495f671a
Erasmous <johnerasmous.soriano@QUANTRICS0625>
2022-04-11 03:46:48 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import List from "./List";
 
-function App() {
+export default function App() {
+  const [number, setNumber] = useState(1);
+  const [dark, setDark] = useState(false);
+
+  const getItems = useCallback(() => {
+    return [number, number + 1, number + 2];
+  }, [number]); //returns a memoized callback function.
+  /*The useCallback and useMemo Hooks are similar. 
+  The main difference is that useMemo returns a memoized value 
+  and useCallback returns a memoized function.*/
+  const theme = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={theme}>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Toggle Theme
+      </button>
+      <List getItems={getItems} />
     </div>
   );
 }
-
-export default App;
